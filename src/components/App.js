2f54aeeaa15f9f1496438208ccb30a499bad69fe
Tomas Puricelli <tomasp1189@gmail.com>
2019-03-23 19:43:40 +0100

    Add first routed section

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import logo from 'assets/marfeel_logo_rgb.svg';
+import mockArticles from 'assets/mockArticles';
 import { Route } from 'react-router-dom';
 import Header from 'components/Header';
+import ArticleList from 'components/Articles/ArticleList';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +11,10 @@ class App extends Component {
 		return (
 			<div className="App">
 				<Header logo={logo} size="sm" />
-				<main>
-					<Route path="/" />
+				<main className="App__content">
+					<Route path="/">
+						<ArticleList articles={mockArticles(4)} />
+					</Route>
 				</main>
 			</div>
 		);
