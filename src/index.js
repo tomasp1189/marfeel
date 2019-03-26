ccad84efdff5f068b43fff0fe783abdd88d9dd9d
Tomas Puricelli <tomasp1189@gmail.com>
2019-03-26 19:08:45 +0100

    Fix routing for github pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from 'components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL}>
 		<App />
 	</BrowserRouter>,
 	document.getElementById('root')
