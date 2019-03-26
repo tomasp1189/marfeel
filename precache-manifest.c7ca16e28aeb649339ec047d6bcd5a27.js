6e0387bd15628b9e24651a893457943e96d6c8d4
Tomas Puricelli <tomasp1189@gmail.com>
2019-03-26 17:40:17 +0100

    Updates

---
+++ precache-manifest.c7ca16e28aeb649339ec047d6bcd5a27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d06370619d6ce8dd5970c30e505e03e3",
+    "url": "/marfeel/static/media/marfeel_logo_rgb.d0637061.svg"
+  },
+  {
+    "revision": "21882e618094c035aae9ab338f50a624",
+    "url": "/marfeel/static/media/lionel-richie.21882e61.svg"
+  },
+  {
+    "revision": "9779cbf2493700d0049d25c2639700d3",
+    "url": "/marfeel/static/media/king.9779cbf2.svg"
+  },
+  {
+    "revision": "dd9b7656d3bb21bec8f9",
+    "url": "/marfeel/static/js/runtime~main.f6979771.js"
+  },
+  {
+    "revision": "92f6021287dc41ea5f64",
+    "url": "/marfeel/static/js/main.d73c8d01.chunk.js"
+  },
+  {
+    "revision": "aaa965edf9c36c17b84e",
+    "url": "/marfeel/static/js/2.c21352c1.chunk.js"
+  },
+  {
+    "revision": "92f6021287dc41ea5f64",
+    "url": "/marfeel/static/css/main.4d10ca43.chunk.css"
+  },
+  {
+    "revision": "0060cb0bc23e21af5737080c373c7659",
+    "url": "/marfeel/index.html"
+  }
+];
